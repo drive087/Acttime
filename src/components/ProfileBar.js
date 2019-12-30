180c444b061764dfbfc0d244225063f97bd01a01
vachirachat <vachirachat@outlook.com>
2019-12-30 13:08:37 +0700

    add css with pixel in ProfileBar but still have bug with responsive

--- src/components/ProfileBar.js
+++ src/components/ProfileBar.js
@@ -1,52 +1,66 @@
 import React, { Component } from 'react';
-import {Grid,Button} from '@material-ui/core';
+import { Grid, Button } from '@material-ui/core';
 import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
 import fire from '../config/Fire';
 import '../style.css';
 
-class ProfileBar extends Component{
-
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        height: 140,
+        width: 100,
+    },
+    control: {
+        padding: theme.spacing(2),
+    },
+}));
+class ProfileBar extends Component {
+    
+    
     constructor(props) {
         super(props);
         this.state = {
-            user:{},
+            user: {},
         }
-      }
+    }
 
-    componentDidMount(){
+    componentDidMount() {
         this.authListener();
     }
 
-    authListener(){
+    authListener() {
         fire.auth().onAuthStateChanged((user) => {
-            if (user){
+            if (user) {
                 this.setState({ user });
-            }else{
-                this.setState({user:null});
+            } else {
+                this.setState({ user: null });
             }
         })
     }
 
-    onLogout(){
+    onLogout() {
         fire.auth().signOut();
     }
-
-    render(){
-        return(
+    
+    render() {
+        return (
             <div>
                 {this.state.user ? (<div>
-                <Button variant='contained' id='RegLogOut' color='primary' 
-          onClick={this.onLogout} >Logout</Button>
-            </div>) : (<div>
-                <Grid direction='column' >
-                    <Grid item><Button href='/Register' variant='outlined' color='inherit' id='RegLogOut'>Register</Button></Grid>
-                    <Grid item><Button href='/login' variant='outlined' color='inherit' id='LoginButton'>Login</Button></Grid>
-                </Grid>
-            </div>)}
+                    <Button variant='contained' id='RegLogOut' color='primary'
+                        onClick={this.onLogout} >Logout</Button>
+                </div>) : (
+                    <div display='inline' id='AllButtonProfile'>
+                        <Button href='/Register' variant='outlined' color='inherit' id='RegLogOut'>Register</Button>
+                        <Button display='inline' href='/login' variant='outlined' color='inherit' id='LoginButton' style={{marginLeft:'10px'}}>Login</Button>
+                    </div>
+                )}
             </div>
         )
 
-        
+
     }
 }
 
